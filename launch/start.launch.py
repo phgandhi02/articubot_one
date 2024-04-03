93aa69e5b481582f529f8b3072bdd9a4b4c9a227
phgandhi02 <phgandh2@ncsu.edu>
2024-04-03 09:22:59 -0400

    made a single start launch file

---
+++ launch/start.launch.py
@@ -0,0 +1,25 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+def generate_launch_description():
+    robot_control = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('articubot_one'), 'launch'),
+            '/launch_robot.launch.py'])
+    )
+
+    joystick = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('articubot_one'), 'launch'),
+            '/joystick.launch.py'])
+    )
+
+    return LaunchDescription([
+        robot_control,
+        joystick
+    ])
+    
